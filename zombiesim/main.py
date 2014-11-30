fbf45244202e441a87acca7e2b957d744709eead
Blaine Buxton <btbuxton@gmail.com>
2014-11-30 16:35:09 -0500

    refactoring

--- zombiesim/main.py
+++ zombiesim/main.py
@@ -27,9 +27,9 @@ def dir_to(origin, dest):
     return (math.cos(angle), math.sin(angle))
 
 def random_direction():
-    x = 1 - (random.random() * 2)
-    y = 1 - (random.random() * 2)
-    return (x,y)
+    def next_rand():
+        return 1 - (random.random() * 2)
+    return (next_rand(), next_rand())
     
 class ActorGroup(pygame.sprite.Group):
     def closest_to(self, other):
@@ -45,13 +45,13 @@ class ActorGroup(pygame.sprite.Group):
             
 class Actor(pygame.sprite.Sprite):
     @classmethod
-    def create_group(clazz, size, width, height):
+    def create_group(clazz, size, rect):
         all_group = ActorGroup()
         for _ in range(size):
             actor = clazz()
             all_group.add(actor)
-            actor.rect.x = random.randrange(width - actor.rect.width)
-            actor.rect.y = random.randrange(height - actor.rect.height)
+            actor.rect.x = random.randrange(rect.x, rect.width - actor.rect.width)
+            actor.rect.y = random.randrange(rect.y, rect.height - actor.rect.height)
         return all_group
     
     def __init__(self, color=WHITE):
@@ -87,49 +87,65 @@ class Human(Actor):
 
 class Field(object):
     def __init__(self, rect):
-        self.rect = rect
-        width = rect.width
-        height = rect.height
-        self.zombies = Zombie.create_group(5, width, height)
-        self.humans = Human.create_group(200, width, height)
+        self.zombies = Zombie.create_group(5, rect)
+        self.humans = Human.create_group(200, rect)
+        
     def register_events(self, events):
         events.every_do(200, lambda: self.zombies.update(self))
         events.every_do(100, lambda: self.humans.update(self))
-    def update(self):
+        
+    def update(self, screen):
         for zombie in self.zombies:
             dead = pygame.sprite.spritecollide(zombie, self.humans, True, collided = pygame.sprite.collide_circle)
             for human in dead:
                 self.turn(human)
-        self.check_edges()
+        self.check_and_fix_edges(screen)
+        
     def draw(self, screen):
         self.zombies.draw(screen)
         self.humans.draw(screen)
+        
     def turn(self, human):
         zombie = Zombie()
         zombie.rect = human.rect
         self.zombies.add(zombie)
-    def check_edges(self):
+        
+    def check_and_fix_edges(self, screen):
+        def check_and_fix(actor, parent_rect):
+            if not parent_rect.contains(actor.rect):
+                if (actor.rect.x < parent_rect.x):
+                    actor.rect.x = parent_rect.width - actor.rect.width
+                if (actor.rect.x > (parent_rect.width - actor.rect.width)):
+                    actor.rect.x = parent_rect.x
+                if (actor.rect.y < parent_rect.y):
+                    actor.rect.y = parent_rect.height - actor.rect.height
+                if (actor.rect.y > (parent_rect.height - actor.rect.height)):
+                    actor.rect.y = parent_rect.y
+                #actor.rect.center = self.rect.center
+        rect = screen.get_rect()
         for each in self.zombies.sprites():
-            if not self.rect.contains(each.rect):
-                each.rect.center = self.rect.center
+            check_and_fix(each, rect)
         for each in self.humans.sprites():
-            if not self.rect.contains(each.rect):
-                each.rect.center = self.rect.center
+            check_and_fix(each, rect)
         
         
 class EventLookup(object):
     def __init__(self):
         self.__mapping__= {}
         self.next_event_id = pygame.USEREVENT
+        
     def add(self, event_type, func=lambda event: None):
         self.__mapping__[event_type] = func
+        
     def next_event_type(self):
         self.next_event_id = self.next_event_id + 1
         return self.next_event_id
+    
     def every_do(self, millis, func=lambda: None):
         event_id = self.next_event_type()
         pygame.time.set_timer(event_id, millis)
         self.add(event_id,lambda _: func())
+        
     def process_events(self):
         for event in pygame.event.get():
             self.__mapping__.get(event.type, lambda event: None)(event)
@@ -151,10 +167,12 @@ def main():
     events.add(pygame.QUIT, mark_done)
     while not main.done:
         events.process_events()
-        field.update()
+        field.update(screen)
+        
         screen.fill(BLACK)
         field.draw(screen)
         pygame.display.flip()
+        
         clock.tick(fps)
     pygame.quit()
     
