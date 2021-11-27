47cd5bb2ad8a2a65294a0f6c9fa806ac85f0ef4c
Blaine Buxton <btbuxton@gmail.com>
2021-11-27 10:25:59 -0500

    before

--- zombiesim/event.py
+++ zombiesim/event.py
@@ -14,10 +14,10 @@ class EventLookup:
         self._keys: dict[int, EventCallback] = {}
         self.next_event_id: int = pygame.USEREVENT
 
-    def add(self, event_type: int, func: EventCallback = lambda event: None):
+    def add(self, event_type: int, func: EventCallback = lambda event: None) -> None:
         self._mapping[event_type] = func
 
-    def add_key_press(self, key, func: EventCallback = lambda event: None):
+    def add_key_press(self, key, func: EventCallback = lambda event: None) -> None:
         def key_func(event: pygame.event.Event) -> None:
             evt_key = event.key
             self._keys.get(evt_key, lambda event: None)(event)
@@ -25,18 +25,18 @@ class EventLookup:
         self._keys[key] = func
         self.add(pygame.KEYDOWN, key_func)
 
-    def func_for(self, event_type: int):
+    def func_for(self, event_type: int) -> EventCallback:
         return self._mapping.get(event_type, lambda event: None)
 
-    def next_event_type(self):
+    def next_event_type(self) -> int:
         self.next_event_id = self.next_event_id + 1
         return self.next_event_id
 
-    def every_do(self, millis: int, func: Runnable = lambda: None):
+    def every_do(self, millis: int, func: Runnable = lambda: None) -> None:
         event_id = self.next_event_type()
         pygame.time.set_timer(event_id, millis)
         self.add(event_id, lambda _: func())
 
-    def process_events(self):
+    def process_events(self) -> None:
         for event in pygame.event.get():
             self._mapping.get(event.type, lambda event: None)(event)
